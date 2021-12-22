fbe2b793f93bc29cc9eea4d576205c744fb579d5
Benjamin Dutta <fijii360@gmail.com>
2021-12-22 11:18:20 +0530

    added ui of pentool and eraser

--- script.js
+++ script.js
@@ -6,10 +6,37 @@ let toolBarFlag = false;
 menuBtn.addEventListener('click', (e) => {
     toolBarFlag = !toolBarFlag;
     if (toolBarFlag) {
-        toolBar.style.display = "none";
-        toolBar.classList.remove("toolBarAni")
-    } else {
         toolBar.style.display = "flex";
         toolBar.classList.add("toolBarAni")
+    } else {
+        toolBar.style.display = "none";
+        toolBar.classList.remove("toolBarAni")
     }
 })
+
+let pen = document.querySelector(".pen");
+
+let penTool = document.querySelector(".pentool");
+let penFlag = false;
+pen.addEventListener('click', (e) => {
+    penFlag = !penFlag;
+    if (penFlag) {
+        penTool.style.display = "flex";
+    } else {
+        penTool.style.display = "none";
+    }
+})
+
+let eraser = document.querySelector(".eraser");
+
+let eraserTool = document.querySelector(".eraserTool");
+let eraserFlag = false;
+eraser.addEventListener('click', (e) => {
+    eraserFlag = !eraserFlag;
+    if (eraserFlag) {
+        eraserTool.style.display = "flex";
+    } else {
+        eraserTool.style.display = "none";
+    }
+})
+
